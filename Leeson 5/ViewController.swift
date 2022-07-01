839d2c1912f6508118780155a1a2b07a4e9c9749
Sergejs Tiselskis <slokas85@gmail.con>
2022-07-01 15:23:58 +0300

    Initial Commit

---
+++ Leeson 5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Leeson 5
+//
+//  Created by John Doe on 01/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
